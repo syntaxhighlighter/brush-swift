d768c88099a2d46dda39452f349c96ef7f65dd6b
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 10:26:40 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Removes Grunt
    * Removes Karma
    * Uses Babel

---
+++ test.js
@@ -0,0 +1,35 @@
+var chai = require('chai');
+var expect = chai.expect;
+var parser = require('parser');
+var Brush = require('./brush');
+var sample = require('fs').readFileSync(`${__dirname}/sample.txt`, 'utf8');
+
+describe('brush-swift', function() {
+  var instance = null;
+
+  before(function() {
+    instance = new Brush();
+  });
+
+  it('has populated code sample', function() {
+    expect(sample).to.not.match(/^Populate/);
+  });
+
+  describe('instance', function() {
+    it('has `regexList`', function() {
+      expect(instance).to.have.property('regexList');
+    });
+  });
+
+  describe('parsing', function() {
+    var matches = null;
+
+    before(function() {
+      matches = parser.parse(sample, instance.regexList);
+    });
+
+    it('can parse', function() {
+      expect(matches).to.have.length.above(0);
+    });
+  });
+});
--- test/shcore-stub.js
+++
@@ -1,2 +0,0 @@
-// this file is here so that legacy brushes will compile because they include `require('shCore')`
-throw new Error("shCore should not execute");
